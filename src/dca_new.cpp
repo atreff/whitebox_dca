d2071812a9300e932601637d035805ea7d5b285d
Alexander Treff <alexander.treff@student.uni-luebeck.de>
2018-03-27 09:13:46 +0000

    updated dca_new

--- src/dca_new.cpp
+++ src/dca_new.cpp
@@ -20,9 +20,6 @@ int glob_maximum = -1;
 // TODO documentation
 void extract_key_byte(int byte, config_t &conf, int correct_key) {
   const int samples = conf.sample_end - conf.sample_start;
-  std::vector<std::vector<std::vector<double>>> attack_graphs(
-      8,
-      std::vector<std::vector<double>>(256, std::vector<double>(samples, 0.0)));
 
   std::array<std::array<std::pair<double, int>, 8>, 256> computed_values2;
   for (int key = 0; key < 256; ++key) {
@@ -50,20 +47,19 @@ void extract_key_byte(int byte, config_t &conf, int correct_key) {
       }
     }
     for (int bitmask = 0; bitmask < 8; ++bitmask) {
-      for (size_t g = 0; g < group1[bitmask].size(); ++g) {
-        group1[bitmask].at(g) /= group1_ctr[bitmask];
+      for (size_t sample = 0; sample < group1[bitmask].size() && sample < group2[bitmask].size(); ++sample) {
+        group1[bitmask].at(sample) /= group1_ctr[bitmask];
+        group2[bitmask].at(sample) /= group2_ctr[bitmask];
       }
-      for (size_t g = 0; g < group2[bitmask].size(); ++g) {
-        group2[bitmask].at(g) /= group2_ctr[bitmask];
-      }
-      auto limit = std::min(group1[bitmask].size(), group2[bitmask].size());
+      auto sample_limit = std::min(group1[bitmask].size(), group2[bitmask].size());
       double maximum_val = -1.0;
-      for (size_t g = 0; g < limit; ++g) {
-        double tmp = std::fabs(group1[bitmask].at(g) - group2[bitmask].at(g));
-        attack_graphs[bitmask].at(key).at(g) = tmp;
+      for (size_t sample = 0; sample < sample_limit; ++sample) {
+        double diff_of_means_at_sample = std::fabs(group1[bitmask].at(sample) - group2[bitmask].at(sample));
         if (tmp > maximum_val) {
-          computed_values2[key].at(bitmask) = std::make_pair(tmp, key);
-          maximum_val = tmp;
+            // set new maximum value for this difference of means trace
+            // when using the corresponding bit
+          computed_values2[key].at(bitmask) = std::make_pair(diff_of_means_at_sample, key);
+          maximum_val = diff_of_means_at_sample;
         }
         if (maximum_val > glob_maximum) {
 
@@ -74,71 +70,66 @@ void extract_key_byte(int byte, config_t &conf, int correct_key) {
     }
   }
   for (int bitmask = 0; bitmask < 8; ++bitmask) {
-    for (int key = 0; key < 256; ++key) {
-      std::sort(std::begin(computed_values2[key]),
-                std::end(computed_values2[key]),
+    for (int keyguess = 0; keyguess < 256; ++keyguess) {
+      std::sort(std::begin(computed_values2[keyguess]),
+                std::end(computed_values2[keyguess]),
                 [](std::pair<double, int> &fst, std::pair<double, int> &snd) {
         return fst.first > snd.first;
       });
     }
   }
-  int bestb_max = -1;
+  int bestbit_max = -1;
   std::vector<std::pair<int, double>> maximum_per_guess;
   std::pair<double, int> gmax = std::pair<double,int>(-1,-1);
-  for (int ik = 0; ik < 256; ik++) {
+  for (int keyguess = 0; keyguess < 256; keyguess++) {
     int bestb = -1;
     std::pair<double, int> currentMax = std::pair<double, int>(-1, -1);
-//    std::cout << "Key " << std::hex << ik << ": [";
-    for (int jb = 0; jb < 8; jb++) {
-//        std::cout << computed_values2[ik].at(jb).first;
-//        if(jb < 7) { std::cout << ','; }
-      if (computed_values2[ik].at(jb).first > currentMax.first) {
-        currentMax = computed_values2[ik].at(jb);
-        bestb = jb;
-        bestb_max = jb;
+    for (int bit = 0; bit < 8; bit++) {
+      if (computed_values2[keyguess].at(bit).first > currentMax.first) {
+        currentMax = computed_values2[keyguess].at(bit);
+        bestb = bit;
+        bestbit_max = bit;
       }
     }
-    maximum_per_guess.push_back(std::pair<int, double>(ik, currentMax.first));
-//    std::cout << "], best: " << currentMax.first << " (bit " << bestb
-//              << ")\n";
+    maximum_per_guess.push_back(std::pair<int, double>(ikey, currentMax.first));
   }
 
-  for(int key = 0; key < 256; ++key)
+  for(int keyguess = 0; keyguess < 256; ++keyguess)
   {
-  std::sort(std::begin(computed_values2[key]),
-          std::end(computed_values2[key]),
+  std::sort(std::begin(computed_values2[keyguess]),
+          std::end(computed_values2[keyguess]),
           [](std::pair<double, int> &fst, std::pair<double, int> &snd) {
           return fst.second > snd.second;
           });
   }
   std::ofstream ofs("stats.out");
-  for(int i = 0; i < 256; i++)
+  for(int keyguess = 0; keyguess < 256; keyguess++)
   {
-      ofs << i << " " << maximum_per_guess[i].second << std::endl;
-      std::cout << i << " -> " << maximum_per_guess[i].second << std::endl;
+      ofs << keyguess << " " << maximum_per_guess[keyguess].second << std::endl;
+      std::cout << keyguess << " -> " << maximum_per_guess[keyguess].second << std::endl;
   }
   std::sort(std::begin(maximum_per_guess), std::end(maximum_per_guess),
           [](std::pair<int, double>& fst, std::pair<int, double>& snd)
           {
             return fst.second > snd.second;
           });
-  int pos = -1;
-  for(int i = 0; i < 256; i++)
+  int position_of_correct_key = -1;
+  for(int keyguess = 0; keyguess < 256; keyguess++)
   {
-      if(maximum_per_guess[i].first == correct_key)
+      if(maximum_per_guess[keyguess].first == correct_key)
       {
-          pos = i;
+          position_of_correct_key = keyguess;
           break;
       }
   }
-  for(int i = 0; i < 256; i++)
+  for(int keyguess = 0; keyguess < 256; keyguess++)
   {
-  //    std::cout << i << " -> " << maximum_per_guess[i].second << std::endl;
+  //    std::cout << keyguess << " -> " << maximum_per_guess[keyguess].second << std::endl;
   }
   std::cout << std::hex << "0x" << maximum_per_guess[0].first // best guess
       << " " << bestb_max // best bit
       << " " << maximum_per_guess[0].second // best peak
-      << " " << std::dec << pos+1 // correct position
+      << " " << std::dec << position_of_correct_key+1 // correct position
       << " " << maximum_per_guess[pos].second << "\n"; // correct peak
 }
 } // namespace dca
