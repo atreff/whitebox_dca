bce913d2cd9f62b401c688a730211e3e5574d4a1
Alexander Treff <alexander.treff@student.uni-luebeck.de>
2018-04-28 23:22:44 +0200

    yet another round of refactoring

--- src/dca_new.cpp
+++ src/dca_new.cpp
@@ -18,15 +18,12 @@ static unsigned int selection_function(uint8_t input, uint8_t keyguess, unsigned
     return (tmp >> bit) & 0x01;
 }
 
-int bestbit = -1;
-int glob_maximum = -1;
-
 // TODO documentation
 void extract_key_byte(unsigned int byte, config_t &conf, unsigned int correct_key)
 {
     const unsigned int samples = conf.sample_end - conf.sample_start;
 
-    std::array<std::array<std::pair<double, unsigned int>, 8>, 256> computed_values2;
+    std::array<std::array<std::pair<double, unsigned int>, 8>, 256> peak_for_key;
     for (unsigned int key = 0; key < 256; ++key)
     {
         std::array<std::array<unsigned int, 8>, 2> group_ctrs;
@@ -38,7 +35,7 @@ void extract_key_byte(unsigned int byte, config_t &conf, unsigned int correct_ke
         {
             for (unsigned int bit = 0; bit < 8; ++bit)
             {
-                auto sel = selection_function(conf.guess_values.at(trace + byte), key, bit);
+                auto sel = selection_function(conf.guess_values[trace + byte], key, bit);
                 group_ctrs[sel][bit]++;
                 for (unsigned int sample = conf.sample_start; sample < conf.sample_end; ++sample)
                 {
@@ -51,25 +48,20 @@ void extract_key_byte(unsigned int byte, config_t &conf, unsigned int correct_ke
         {
             for (size_t sample = 0; sample < groups[0][bit].size() && sample < groups[1][bit].size(); ++sample)
             {
-                groups[0][bit].at(sample) /= group_ctrs[0][bit];
-                groups[1][bit].at(sample) /= group_ctrs[1][bit];
+                groups[0][bit][sample] /= group_ctrs[0][bit];
+                groups[1][bit][sample] /= group_ctrs[1][bit];
             }
-            auto sample_limit = std::min(groups[0][bit].size(), groups[1][bit].size());
-            double maximum_val = -1.0;
-            for (size_t sample = 0; sample < sample_limit; ++sample)
+            auto max_samples = std::min(groups[0][bit].size(), groups[1][bit].size());
+            double maximum_dom = -1.0;
+            for (size_t sample = 0; sample < max_samples; ++sample)
             {
-                double diff_of_means_at_sample = std::fabs(groups[0][bit].at(sample) - groups[1][bit].at(sample));
-                if (diff_of_means_at_sample > maximum_val)
+                double sample_diff = std::fabs(groups[0][bit][sample] - groups[1][bit][sample]);
+                if (sample_diff > maximum_dom)
                 {
                     // set new maximum value for this difference of means trace
                     // when using the corresponding bit
-                    computed_values2[key].at(bit) = std::make_pair(diff_of_means_at_sample, key);
-                    maximum_val = diff_of_means_at_sample;
-                }
-                if (maximum_val > glob_maximum)
-                {
-                    glob_maximum = maximum_val;
-                    bestbit = bit;
+                    peak_for_key[key][bit] = std::make_pair(sample_diff, key);
+                    maximum_dom = sample_diff;
                 }
             }
         }
@@ -78,37 +70,38 @@ void extract_key_byte(unsigned int byte, config_t &conf, unsigned int correct_ke
     {
         for (unsigned int keyguess = 0; keyguess < 256; ++keyguess)
         {
-            std::sort(std::begin(computed_values2[keyguess]),
-                      std::end(computed_values2[keyguess]),
+            std::sort(std::begin(peak_for_key[keyguess]),
+                      std::end(peak_for_key[keyguess]),
                       [](std::pair<double, unsigned int> &fst, std::pair<double, unsigned int> &snd)
                       {
                           return fst.first > snd.first;
                       });
         }
     }
+    
+    // calculate the highest peak for each key guess
     int bestbit_max = -1;
-    std::vector<std::pair<unsigned int, double>> maximum_per_guess;
-    std::pair<double, int> gmax = std::pair<double,int>(-1,-1);
+    std::vector<std::pair<unsigned int, double>> maximum_per_guess; // maps from key guess => best peak over all 8 bits
     for (unsigned int keyguess = 0; keyguess < 256; keyguess++)
     {
-        int bestb = -1;
-        std::pair<double, int> currentMax = std::pair<double, int>(-1, -1);
+        std::pair<double, int> current_peak = std::pair<double, int>(-1, -1);
         for (unsigned int bit = 0; bit < 8; bit++)
         {
-            if (computed_values2[keyguess].at(bit).first > currentMax.first)
+            if (peak_for_key[keyguess][bit].first > current_peak.first)
             {
-                currentMax = computed_values2[keyguess].at(bit);
-                bestb = bit;
+                current_peak = peak_for_key[keyguess][bit];
                 bestbit_max = bit;
             }
         }
-        maximum_per_guess.push_back(std::pair<int, double>(keyguess, currentMax.first));
+        maximum_per_guess.push_back(std::pair<int, double>(keyguess, current_peak.first));
     }
+    // end: calculate the highest peak for each key guess
+    
 
     for(unsigned int keyguess = 0; keyguess < 256; ++keyguess)
     {
-        std::sort(std::begin(computed_values2[keyguess]),
-              std::end(computed_values2[keyguess]),
+        std::sort(std::begin(peak_for_key[keyguess]),
+              std::end(peak_for_key[keyguess]),
               [](std::pair<double, unsigned int> &fst, std::pair<double, unsigned int> &snd) {
                   return fst.second > snd.second;
                });
@@ -124,6 +117,9 @@ void extract_key_byte(unsigned int byte, config_t &conf, unsigned int correct_ke
           {
             return fst.second > snd.second;
           });
+          
+    // look for the position of the given correct key
+    // if you give a wrong key, you'll get the position of that instead
     int position_of_correct_key = -1;
     for(unsigned int keyguess = 0; keyguess < 256; keyguess++)
     {
